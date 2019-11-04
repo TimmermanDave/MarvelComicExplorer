393cc1acfe72f39ce6a1b6a79f4f901b1e733250
D. Timmerman <d.timmerman@quomd.nl>
2019-11-04 09:29:20 +0100

    code cleaning

--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,29 @@ const path = require('path');
 const router = express.Router();
 const request = require('request');
 const crypto = require('crypto');
-const pubkey = 'cf6fba0fe2ff075b1a326f1d3d8263f5';
-const privkey = '7a52e6356ee5f3eb9d48ef0ade465c5abdb2e6b5';
 
-/*
-Request Url: http://gateway.marvel.com/v1/public/comics
-Request Method: GET
-Params: {
-  "apikey": "your api key",
-  "ts": "a timestamp",
-  "hash": "your hash"
+const tokenize = () => {
+  const pubkey = 'cf6fba0fe2ff075b1a326f1d3d8263f5';
+  const privkey = '7a52e6356ee5f3eb9d48ef0ade465c5abdb2e6b5';
+  const ts = Date.now();
+  const hash = crypto.createHash('md5');
+  const data = hash.update(ts + privkey + pubkey, 'utf-8');
+  const genHash = data.digest('hex');
+  return { ts, apikey: pubkey, hash: genHash };
 }
-Headers: {
-  Accept: * /*
+
+const call = (req, res, next) => {
+    const tokens = tokenize();
+    const qs = { ...tokens, ...req.query };
+    const uri = `http://gateway.marvel.com/v1/public/${req.params.uri}`;
+    const options = { uri, qs };
+    try {
+      request(options).pipe(res);
+    } catch(err) {
+      next(err);
+    }
 }
-*/
 
-const getComics = (req, res, next) => {
-    const ts = Date.now();
-    const hash = crypto.createHash('md5');
-    const data = hash.update(ts+privkey+pubkey, 'utf-8');
-    const gen_hash= data.digest('hex');
-    const qs = { apikey: pubkey, ts, hash: gen_hash }
-    request({
-        uri: 'http://gateway.marvel.com/v1/public/comics',
-        qs,
-    })
-    .pipe(res);
-}    
-
-router
-//   .route('/api/v1/comics/:id')
-  .route('/api/v1/comics')
-  .get(getComics);
+router.route('/api/v1/:uri').get(call);
 
 module.exports = router;
