3aa1e96deb4ccab30f34e6f7f72851e0cadb50f1
TimmermanDave <dave@frontmen.nl>
2020-02-06 16:06:42 +0100

    added application main drawer

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@
     import Icon from '@smui/textfield/icon/index';
 	import HelperText from '@smui/textfield/helper-text/index';
 	import Button, {Group, GroupItem, Label} from '@smui/button';
+	import Drawer, {AppContent, Content, Header, Title, Subtitle, Scrim} from '@smui/drawer';
+	import List, {Item, Text, Graphic, Separator, Subheader} from '@smui/list';
+	import H6 from '@smui/common/H6.svelte';
+	
 	import { categories, currentPath } from './stores';
 
 	import Box from './components/Box';
@@ -11,7 +15,20 @@
 
 	export let appName;
 
+	// search
 	let searchValue = '';
+
+	// drawer
+	let myDrawer;
+	let myDrawerOpen = false;
+	let active = 'Gray Kittens';
+
+	function setActive(value) {
+		active = value;
+		myDrawerOpen = false;
+	}
+
+
 </script>
 	
 <style>
@@ -46,14 +63,15 @@
 	<Box elevation="5">
 		<h1>{appName}</h1>
 		<h4>/{$currentPath}</h4>
+
 		<Group variant="outlined">
-			<Button on:click={() => clicked++} variant="outlined"><Label>One</Label></Button>
-			<Button on:click={() => clicked++} variant="outlined"><Label>Two</Label></Button>
-			<Button on:click={() => clicked++} variant="outlined"><Label>Three</Label></Button>
+			<Button on:click={() => {}} variant="outlined"><Label>One</Label></Button>
+			<Button on:click={() => {}} variant="outlined"><Label>Two</Label></Button>
+			<Button on:click={() => {}} variant="outlined"><Label>Three</Label></Button>
 		</Group>
 
 		<div>
-			<Textfield fullwidth class="shaped-outlined" variant="outlined" withTrailingIcon bind:value={searchValue} label="Search" input$aria-controls="helper-text-search" input$aria-describedby="helper-text-search">
+			<Textfield class="shaped-outlined" variant="outlined" withTrailingIcon bind:value={searchValue} label="Search" input$aria-controls="helper-text-search" input$aria-describedby="helper-text-search">
 				<Icon class="material-icons">search</Icon>
 			</Textfield>
 			<HelperText id="helper-text-search">Search the Marvel database</HelperText>
@@ -61,9 +79,40 @@
 	</Box>
 </section>
 
-<section class="menu">
-	<Box>
-	</Box>
+<section class="drawer-container">
+	<Drawer variant="dismissible" bind:this={myDrawer} bind:open={myDrawerOpen}>
+	<Header>
+		<Title>Super Drawer</Title>
+		<Subtitle>It's the best drawer.</Subtitle>
+	</Header>
+	<Content>
+		<List>
+		<Item href="javascript:void(0)" on:click={() => setActive('Gray Kittens')} activated={active === 'Gray Kittens'}>
+			<Text>Gray Kittens</Text>
+		</Item>
+		<Item href="javascript:void(0)" on:click={() => setActive('A Space Rocket')} activated={active === 'A Space Rocket'}>
+			<Text>A Space Rocket</Text>
+		</Item>
+		<Item href="javascript:void(0)" on:click={() => setActive('100 Pounds of Gravel')} activated={active === '100 Pounds of Gravel'}>
+			<Text>100 Pounds of Gravel</Text>
+		</Item>
+		<Item href="javascript:void(0)" on:click={() => setActive('All of the Shrimp')} activated={active === 'All of the Shrimp'}>
+			<Text>All of the Shrimp</Text>
+		</Item>
+		<Item href="javascript:void(0)" on:click={() => setActive('A Planet with a Mall')} activated={active === 'A Planet with a Mall'}>
+			<Text>A Planet with a Mall</Text>
+		</Item>
+		</List>
+	</Content>
+	</Drawer>
+
+	<AppContent class="app-content">
+	<main class="main-content">
+		<Button on:click={() => myDrawerOpen = !myDrawerOpen}><Label>Toggle Drawer</Label></Button>
+		<br />
+		<pre class="status">Active: {active}</pre>
+	</main>
+	</AppContent>
 </section>
 
 <section class="main">
