a63a77371c5f0db995b7b5c56ad500124f25916e
TimmermanDave <dave@frontmen.nl>
2020-02-06 17:31:01 +0100

    moved main menu items to main content of drawer

--- src/App.svelte
+++ src/App.svelte
@@ -49,59 +49,67 @@
 </script>
 	
 <style>
-	:global(.shaped) {
+	* :global(.shaped) {
 		border-radius: 16px 16px 0 0;
 	}
-	:global(.shaped-outlined .mdc-text-field__input) {
+	* :global(.shaped-outlined .mdc-text-field__input) {
 		padding-left: 32px;
 		padding-right: 32px;
 	}
-	:global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__leading) {
+	* :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__leading) {
 		border-radius: 28px 0 0 28px;
 		width: 28px;
 	}
-	:global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__trailing) {
+	* :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__trailing) {
 		border-radius: 0 28px 28px 0;
 	}
-	:global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__notch) {
+	* :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__notch) {
 		max-width: calc(100% - 28px * 2);
 	}
-	:global(.shaped-outlined + .mdc-text-field-helper-line) {
+	* :global(.shaped-outlined + .mdc-text-field-helper-line) {
 		padding-left: 32px;
 		padding-right: 28px;
+	}
+
+	.drawer-container {
+		position: relative;
+		display: flex;
+		height: 100%;
+		max-width: 100%;
+		border: 1px solid rgba(0,0,0,.1);
+		overflow: hidden;
+		z-index: 0;
+	}
+	* :global(.mdc-drawer--modal, .mdc-drawer-scrim) {
+		/* This is not needed for a page-wide modal. */
+		position: absolute;
+	}
+	* :global(.app-content) {
+		flex: auto;
+		overflow: auto;
+		position: relative;
+		flex-grow: 1;
+	}
+	.main-content {
+		overflow: auto;
+		padding: 16px;
+		height: 100%;
+		box-sizing: border-box;
 	}
 </style>
 
 <svelte:head>
-  <title>Home</title>
+  <title>Home - Marvel Universe Explorer</title>
 </svelte:head>
-
-<section class="heading">
-	<Box elevation="5">
-		<h1>{appName}</h1>
-		<h4>/{$currentPath}</h4>
-
-		<Group variant="outlined">
-			<Button on:click={() => {}} variant="outlined"><Label>One</Label></Button>
-			<Button on:click={() => {}} variant="outlined"><Label>Two</Label></Button>
-			<Button on:click={() => {}} variant="outlined"><Label>Three</Label></Button>
-		</Group>
-
-		<div>
-			<Textfield class="shaped-outlined" variant="outlined" withTrailingIcon bind:value={searchValue} label="Search" input$aria-controls="helper-text-search" input$aria-describedby="helper-text-search">
-				<Icon class="material-icons">search</Icon>
-			</Textfield>
-			<HelperText id="helper-text-search">Search the Marvel database</HelperText>
-		</div>
-	</Box>
-</section>
 
 <section class="drawer-container">
 	<Drawer variant="dismissible" bind:this={myDrawer} bind:open={myDrawerOpen}>
 		<Header>
-			<Title>Categories</Title>
+			<Title>{appName}</Title>
 			<Subtitle>All searchable categories</Subtitle>
+			<h4>/{$currentPath}</h4>
 		</Header>
+		
 		<Content>
 			<!-- <List>
 			<Item href="javascript:void(0)" on:click={() => setActive('Gray Kittens')} activated={active === 'Gray Kittens'}>
@@ -126,9 +134,24 @@
 
 	<AppContent class="app-content">
 		<main class="main-content">
-			<Button on:click={() => myDrawerOpen = !myDrawerOpen}><Label>Toggle Drawer</Label></Button>
+			
+			<Group variant="outlined">
+				<Button on:click={() => myDrawerOpen = !myDrawerOpen} variant="outlined"><Label>{myDrawerOpen ? 'Close' : 'Open'} Menu</Label></Button>
+				<Button on:click={() => {}} variant="outlined"><Label>One</Label></Button>
+				<Button on:click={() => {}} variant="outlined"><Label>Two</Label></Button>
+				<Button on:click={() => {}} variant="outlined"><Label>Three</Label></Button>
+			</Group>
+
+			<div>
+				<Textfield class="shaped-outlined" variant="outlined" withTrailingIcon bind:value={searchValue} label="Search" input$aria-controls="helper-text-search" input$aria-describedby="helper-text-search">
+					<Icon class="material-icons">search</Icon>
+				</Textfield>
+				<HelperText id="helper-text-search">Search the Marvel database</HelperText>
+			</div>
+
 			<!-- <br /> -->
 			<!-- <pre class="status">Active: {active}</pre> -->
+
 			<SearchResultFirstLevel />
 			<SearchResultSecondLevel />
 		</main>
